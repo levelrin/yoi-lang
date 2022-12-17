3e5555c885a06470ed64a3eabdc39fe8a92edb78
Rin <levelrin@gmail.com>
2022-12-18 01:57:41 +0900

    Initialize the project.

---
+++ src/main/java/com/levelrin/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2022 Rin (https://www.levelrin.com)
+ *
+ * This file has been created under the terms of the MIT License.
+ * See the details at https://github.com/levelrin/yoi-lang/blob/main/LICENSE
+ */
+
+package com.levelrin;
+
+/**
+ * Entry point of the application.
+ */
+@SuppressWarnings({"HideUtilityClassConstructor", "PMD.UseUtilityClass"})
+public final class Main {
+
+    /**
+     * Main method.
+     * @param args Command line arguments.
+     */
+    public static void main(final String... args) {
+        // todo: develop app.
+    }
+
+}
---
+++ src/main/java/com/levelrin/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2022 Rin (https://www.levelrin.com)
+ *
+ * This file has been created under the terms of the MIT License.
+ * See the details at https://github.com/levelrin/yoi-lang/blob/main/LICENSE
+ */
+
+/**
+ * Root package.
+ */
+package com.levelrin;
